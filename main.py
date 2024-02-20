f37b499c3982c5d62b5ad599dee22f2ccae1df03
MARCUS-IMPERATOR <160549731+MARCUS-IMPERATOR@users.noreply.github.com>
2024-02-20 16:37:38 +0100

    Update main.py
    another one :(

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 if __name__ == "__main__":
+    from app.routes import task 
     import uvicorn
-    uvicorn.run(prediction_app, host="0.0.0.0", port=8000)
+    uvicorn.run(task, host="0.0.0.0", port=8000)
